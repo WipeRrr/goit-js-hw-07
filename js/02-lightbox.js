e50448e8119a7ced42f4927d00634a090134acf1
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-18 22:39:09 +0200

    2 task start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,27 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
-
 
 const paletteContainer = document.querySelector(`.gallery`);
 const imgGallery = createImgGallery(galleryItems);
-
-paletteContainer.insertAdjacentHTML(`beforeend`, imgGallery);
 
 function createImgGallery(galleryItems) {
   return galleryItems
     .map(({preview, original, description}) => {
       return `
-        <div class ="gallery__item ">
-        <a class="gallery__link" href = "">
-      <img
-  class="gallery__image"
-  src="${preview}"
-  data-src="${original}"
-  alt="${description}"
-/>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image"   loading="lazy" src="${preview}" alt="${description}" />
 </a>
-</div>
 `;
     })
     .join('');
 }
+
+paletteContainer.insertAdjacentHTML(`beforeend`, imgGallery);
