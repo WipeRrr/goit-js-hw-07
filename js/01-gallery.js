78ce3dba461d8be2658af35e86395157f18369f0
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-18 22:30:29 +0200

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1
+const paletteContainer = document.querySelector(`.gallery`);
+const imgGallery = createImgGallery(galleryItems);
+
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({preview, original, description}) => {
+      return `
+        <div class ="gallery__item lazyload ">
+        <a class="gallery__link" href="${original}">
+      <img
+  class="gallery__image"
+  loading="lazy"
+  src="${preview}"
+  alt="${description}"
+  data-source ="${original}"
+
+/>
+</a>
+</div>
+`;
+    })
+    .join('');
+}
+
+paletteContainer.insertAdjacentHTML(`beforeend`, imgGallery);
+
+// 2
+paletteContainer.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== `IMG`) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  window.addEventListener('keydown', onEscKeyPress);
+
+  function onEscKeyPress(evt) {
+    if (evt.code === 'Escape') {
+      window.removeEventListener('keydown', onEscKeyPress);
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const paletteContainer = document.querySelector(`.gallery`);
+const imgGallery = createImgGallery(galleryItems);
+
+paletteContainer.insertAdjacentHTML(`beforeend`, imgGallery);
+
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({preview, original, description}) => {
+      return `
+        <div class ="gallery__item ">
+        <a class="gallery__link" href = "">
+      <img
+  class="gallery__image"
+  src="${preview}"
+  data-src="${original}"
+  alt="${description}"
+/>
+</a>
+</div>
+`;
+    })
+    .join('');
+}
